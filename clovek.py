bec1c0d6dbf792bdf520819c87485afa3e5ca483
anjakisek <anja.kisek@gmail.com>
2017-04-10 21:02:14 +0200

    Dodala cloveka kot igralca

---
+++ clovek.py
@@ -0,0 +1,12 @@
+class Clovek():
+    def __init__(self, gui):
+        self.gui = gui
+
+    def igraj(self):
+        pass
+
+    def prekini(self):
+        pass
+
+    def klik(self, p):
+        self.gui.naredi_potezo(p)
