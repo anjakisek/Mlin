c10ca723dae229b9a4c42d5a7cfc0f745e064241
anjakisek <anja.kisek@gmail.com>
2017-05-10 14:56:07 +0200

    Koncna verzija, upam.

--- racunalnik_igralec.py
+++ racunalnik_igralec.py
@@ -6,39 +6,45 @@ import time
 class Racunalnik():
     def __init__(self, gui, algoritem):
         self.gui = gui
-        self.algoritem = algoritem
+        self.algoritem = algoritem #izbran algoritem, minimax ali alphabeta
         self.mislec = None #vlakno za razmisljanje
+        self.je_treba_prekiniti = False
 
     def igraj(self):
         '''V vzporednem vlaknu sprozi razmislanje o novi potezi'''
-        #sprozimo razmisljanje
+        #Sprozimo razmisljanje
         self.mislec = threading.Thread(
             target=lambda: self.algoritem.izracunaj_potezo(
                 self.gui.igra.kopija()))
 
-        #pozenemo vlakno
+        #Pozenemo vlakno
         self.mislec.start()
         self.zacni_meriti_cas =time.time()
 
-        #preverjamo na vsake 100ms, ali je mislec ze razmislil
+        #Preverjamo na vsake 100ms, ali je mislec ze razmislil
         self.gui.plosca.after(100, self.preveri_potezo)
 
     def preveri_potezo(self):
         '''Po dolocenem casu preveri, ali je vlakno ze naslo optimalno potezo in
     potezo naredi s primernim zamikom.'''
 
-        #vsake 100ms preveri, ce je mislec ze koncal
+        #Vsake 100ms preveri, ce je mislec ze koncal
         if self.algoritem.poteza is not None:
 
             self.pretekli_cas = time.time() - self.zacni_meriti_cas
             if self.pretekli_cas < 1/2:
+                #Poskrbi, da racunalnik poteze ne potegne prehitro, saj je
+                #takim potezam na plosci tezko slediti
                 time.sleep(2/3)
-            # XXX potezo naredimo samo, če nismo bili prekinjeni
-            self.gui.naredi_potezo(self.algoritem.poteza)
+            #Ce smo tacas prekinili igro, poteze ne naredimo
+            if not self.je_treba_prekiniti:
+                self.gui.naredi_potezo(self.algoritem.poteza)
         else:
+            #Ce poteze se ni izracunal, cez cas preveri, ce je ze nared
             self.gui.plosca.after(100, self.preveri_potezo)
 
     def prekini(self):
+        self.je_treba_prekiniti = True
         if self.mislec:
             # Algoritmu sporocimo, naj neha
             self.algoritem.prekini()
